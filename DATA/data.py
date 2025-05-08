45d70be34810e89c3bd72ec14f70228c99ba3510
Mark Brezina <77184504+COPtoLON@users.noreply.github.com>
2025-05-08 09:58:40 +0100

    Create data.py

---
+++ DATA/data.py
@@ -0,0 +1,6 @@
+client = Spot()
+
+api_key = 'vBrHMZUCDxlfNiAreBj02aUrymSFMGyl1AwJNAP0O7qVlvh07Drq7qQwfQlbYGeS'
+api_secret = 'SdzCkiFE5zNjkSvptRVpxQBxlUWZWYrjnRGLp5tzhzJiat79vymOH127zaKHnnCh'
+
+client = Spot(api_key, api_secret)
