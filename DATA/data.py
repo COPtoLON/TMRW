77b040ad6d197b5e48afbe8faa6a84fbb0c8d93a
Mark Brezina <77184504+COPtoLON@users.noreply.github.com>
2025-05-08 13:35:00 +0100

    Update data.py

--- DATA/data.py
+++ DATA/data.py
@@ -1,6 +1,18 @@
-client = Spot()
 
-api_key = 'vBrHMZUCDxlfNiAreBj02aUrymSFMGyl1AwJNAP0O7qVlvh07Drq7qQwfQlbYGeS'
-api_secret = 'SdzCkiFE5zNjkSvptRVpxQBxlUWZWYrjnRGLp5tzhzJiat79vymOH127zaKHnnCh'
+# Coinbase API
+# username: mark@brezina.dk
+# password: Ink-wire1x
 
-client = Spot(api_key, api_secret)
+from coinbase.rest import RESTClient
+api_key = "organizations/3d3c116b-2b78-4e96-a5c5-671cc6d1857a/apiKeys/04dd8c95-f093-4485-a03d-4cb10dd88be2"
+api_secret = "-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIEPljrHWwou2He+fBUmS79LPvL33TBV8mVciFp7s6kcAoAoGCCqGSM49\nAwEHoUQDQgAEZOqZjofgcT3cb90WM3BpckqkM8+tV1QOwTDNTC49QJQ39sxMhJu0\nOj3a/C81uosF+CQi2oqEij7OHGluo0DrMg==\n-----END EC PRIVATE KEY-----\n"
+client = RESTClient(api_key=api_key, api_secret=api_secret)
+
+# Kraken API
+# username: mark@brezina.dk
+# password: Ink-wire123x
+
+
+
+
+
